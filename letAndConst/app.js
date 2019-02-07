63e42c2c74cf5e24157e7d95a77c425790ef1912
Abel Fernandez <abelfergodevelop@gmail.com>
2019-02-07 21:52:45 +0100

    Clase 11

---
+++ letAndConst/app.js
@@ -0,0 +1,8 @@
+"use strict";
+var mensaje = "hola";
+var OPCIONES = "todas";
+if (true) {
+    var mensaje_1 = "adios";
+    // OPCIONES = "123";    //No deja cambiarla porque es una constante
+}
+console.log(mensaje);
---
+++ letAndConst/app.ts
@@ -0,0 +1,9 @@
+let mensaje = "hola";
+const OPCIONES = "todas";
+
+if(true) {
+    let mensaje = "adios";
+    // OPCIONES = "123";    //No deja cambiarla porque es una constante
+}
+
+console.log(mensaje);
