12008796520b780e9f963077113955c9c4822add
Abel Fernandez <abelfergodevelop@gmail.com>
2019-02-07 22:38:10 +0100

    Clase 15

---
+++ funciones/app.js
@@ -0,0 +1,16 @@
+"use strict";
+//tsc -init
+//tsc -w
+function activar(quien, objeto, momento) {
+    if (objeto === void 0) { objeto = "batiseñal"; }
+    var mensaje;
+    if (momento) {
+        mensaje = quien + " activ\u00F3 la " + objeto + " en la " + momento;
+    }
+    else {
+        mensaje = quien + " activ\u00F3 la " + objeto;
+    }
+    console.log(mensaje);
+}
+;
+activar("Gordon", "batiseñal", "tarde");
